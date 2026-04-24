84abcc6fa15cb33dfc73ef95c4ffe4a38c31d735
Oscar-Murillo <oscaralexandermurillo500@gmail.com>
2026-04-24 17:43:00 -0600

    refactor: :bug: force-dynamic para el despliegue

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import BenefitsBar from "@/components/BenefitsBar";
 import FeaturedCollection from "@/components/FeaturedCollection";
 import CategoryGrid from "@/components/CategoryGrid";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return (
     <>
